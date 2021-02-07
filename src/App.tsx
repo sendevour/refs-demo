f5945ef818053766466626fda65d43e365e8e142
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-07 22:54:38 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import * as React from 'react';
 
 export interface Props {
-  
+
 }
- 
+
 export interface State {
-  
 }
- 
+
 class App extends React.Component<Props, State> {
-  
-  render() { 
-    return ( <h3>Learning Refs</h3> );
+  private searchRef: React.RefObject<HTMLInputElement>;
+
+  constructor(props: Props) {
+    super(props);
+    this.searchRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.searchRef.current?.focus();
+  }
+
+
+  render() {
+    return (
+      <>
+        <div className='container'>
+          <h3>Learning Refs</h3>
+          <hr />
+          <input type="text" className='form-control' ref={this.searchRef} />
+        </div>
+      </>
+    );
   }
 }
 
